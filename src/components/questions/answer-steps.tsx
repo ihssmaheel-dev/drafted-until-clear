28a156654a47e911d1e7b21e094f75800e38dcea
Mohamed Ismail S <ihssmaheel@gmail.com>
2026-08-16 20:30:42 +0530

    style: reduce spacing between timeline steps for a tighter, neater flow

--- src/components/questions/answer-steps.tsx
+++ src/components/questions/answer-steps.tsx
@@ -25,7 +25,7 @@ export function Step({ icon, title, children }: StepProps) {
       initial={{ opacity: 0, x: -10 }}
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.3 }}
-      className="relative pl-10 pb-8 last:pb-2"
+      className="relative pl-10 pb-5 last:pb-2"
     >
       {/* Centered exactly on the 2px line (size-8 = 32px; center of 2px border is at -1px from padding box. -17px + 16px = -1px) */}
       <div className="absolute -left-[17px] top-0 flex size-8 items-center justify-center rounded-full border border-border bg-card text-text-muted">
